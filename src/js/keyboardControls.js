4f77527f0c09faf16567f26f7c9af23cc58ecf1f
Ben Hall <benhall22@hotmail.co.uk>
2017-01-08 17:21:11 +0000

    style:keyboardControls

--- src/js/keyboardControls.js
+++ src/js/keyboardControls.js
@@ -1,23 +1,13 @@
 module.exports = ({changeWarp, changeV}) => document.onkeydown = e => {
   switch (e.keyCode) {
-    case 27:
-      // escape
-      return window.location.reload()
+    case 27: return location.reload()
     case 37:
-    case 65:
-      // left
-      return changeWarp(-0.0005)
+    case 65: return changeWarp(-0.0005)
     case 39:
-    case 68:
-      // right
-      return changeWarp(0.0005)
+    case 68: return changeWarp(0.0005)
     case 38:
-    case 87:
-      // up
-      return changeV(0.2)
+    case 87: return changeV(0.2)
     case 40:
-    case 83:
-      // right
-      return changeV(-0.2)
+    case 83: return changeV(-0.2)
   }
 }
